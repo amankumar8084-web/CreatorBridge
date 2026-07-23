dc49607abeea15a30106262cadb766cde05c8412
amankumar8084-web <amankumar8084227421@gmail.com>
2026-07-23 22:37:11 +0530

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const startServer = async () => {
     await initRedis();
     initSocket(io);
     
-    const PORT = process.env.PORT || 4000;
+    const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => {
       console.log(` Server running on port ${PORT}`);
     });
